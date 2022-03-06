0c6df01aaf029d49480fe1ed8a095dc866111fff
Walter Agazzi <tag2015@gmail.com>
2022-03-06 01:17:00 +0100

    Tabs to spaces. Removed unused AMIGA ifdef.

--- os.h
+++ os.h
@@ -1,7 +1,7 @@
 /*
-	 This file is part of Tabboz Simulator.
+     This file is part of Tabboz Simulator.
 
-	 Tabboz Simulator is free software: you can redistribute it and/or modify
+    Tabboz Simulator is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -12,41 +12,27 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-	 along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+    along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// -------------------------------------------------------------------
-// Definizioni per i vari sistemi operativi...
+/* Definizioni per i vari sistemi operativi... */
 
 // #define TABBOZ_LINUX
-// #define TABBOZ_AMIGA
 // #define TABBOZ_WIN16
 #define TABBOZ_WIN32
 
-// Linux -------------------------------------------------------------
-
+// Linux
 #ifdef TABBOZ_LINUX
-#include <gtk/gtk.h>
-etc...
 #endif
 
-// Amiga -------------------------------------------------------------
-
-#ifdef TABBOZ_AMIGA
-etc...
-#endif
-
-// Windows 16bit -----------------------------------------------------
-
+// Windows 16bit
 #ifdef TABBOZ_WIN16
 #define TABBOZ_WIN
 #include <windows.h>
 #endif
 
-// Windows 32bit -----------------------------------------------------
-
+// Windows 32bit
 #ifdef TABBOZ_WIN32
 #define TABBOZ_WIN
 #include <windows.h>
 #endif
-
