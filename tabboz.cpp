444298db3045b46ad4dc2a8b19a088c3eddb9d8b
Walter Agazzi <tag2015@gmail.com>
2022-03-15 19:59:37 +0100

    Tabs to spaces. Add new files to start project

---
+++ tabboz.cpp
@@ -0,0 +1,11 @@
+#include "tabboz.h"
+
+Tabboz::Tabboz(wxWindow* parent)
+    : TabbozBase(parent)
+{
+}
+
+Tabboz::~Tabboz()
+{
+}
+
---
+++ tabboz.h
@@ -0,0 +1,11 @@
+#ifndef TABBOZ_H
+#define TABBOZ_H
+#include "wxcrafter.h"
+
+class Tabboz : public TabbozBase
+{
+public:
+    Tabboz(wxWindow* parent);
+    virtual ~Tabboz();
+};
+#endif // TABBOZ_H
