d0036624e5f165cfc65b64a76949ebc4052fd948
Walter Agazzi <tag2015@gmail.com>
2022-03-24 22:52:33 +0100

    checksums functions cleanup

--- proteggi.c
+++ proteggi.c
@@ -1,10 +1,10 @@
-/* Tabboz Simulator			*/
-/* (C) Copyright 1998-1999 by Andrea Bonomi	*/
+/*             Tabboz Simulator             */
+/* (C) Copyright 1998-1999 by Andrea Bonomi */
 
 /*
-	 This file is part of Tabboz Simulator.
+    This file is part of Tabboz Simulator.
 
-	 Tabboz Simulator is free software: you can redistribute it and/or modify
+    Tabboz Simulator is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -15,7 +15,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-	 along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+    along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // ------------------------------------------------------------------------------------------------
@@ -27,8 +27,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <fcntl.h>
-#include <sys\stat.h>
+//#include <fcntl.h>
+//#include <sys\stat.h>
 
 #include "zarrosim.h"
 
@@ -141,15 +141,15 @@ long 	 int		xxx2=0;
 /*
 void clock_r1(u_long i1)
 {
-	 if (i1 > 255 )
-		 new_counter=crctab[( (i1 % 255) >> 8) & 255] ^ (i1 << 8) ^ new_counter + 1999;
-	 new_counter=crctab[(i1 >> 8) & 255] ^ (i1 << 8) ^ new_counter + 79;
+    if (i1 > 255 )
+        new_counter=crctab[( (i1 % 255) >> 8) & 255] ^ (i1 << 8) ^ new_counter + 1999;
+    new_counter=crctab[(i1 >> 8) & 255] ^ (i1 << 8) ^ new_counter + 79;
 }
 */
 
 // Nuova funzione ------------------------------------------------------------------
 // 10 Marzo 2000
-// Correzzioni a tutte le procedure del checksum.
+// Correzioni a tutte le procedure del checksum.
 
 u_long new_counter;
 static char	p_ruota1;
@@ -159,23 +159,22 @@ static int  ruota2[5] = { 0xb47f,0xc37b,0x1070,0x1999, 0xfb1e };
 
 void clock_r1(u_long i1)
 {
-	u_long	a1,a2;
+    u_long a1,a2;
 
-	a1= (i1 | ruota1[p_ruota1]) * (p_ruota2 + 1);
-	p_ruota1++;
-	if (p_ruota1 > 7) p_ruota1=0;
+    a1 = (i1 | ruota1[p_ruota1]) * (p_ruota2 + 1);
+    p_ruota1++;
+    if (p_ruota1 > 7) p_ruota1=0;
 
-	a2= (a1 & ruota2[p_ruota2]) * (p_ruota1 + 1);
-	p_ruota2++;
-	if (p_ruota2 > 4) p_ruota2=0;
+    a2 = (a1 & ruota2[p_ruota2]) * (p_ruota1 + 1);
+    p_ruota2++;
+    if (p_ruota2 > 4) p_ruota2=0;
 
-	if (a2 >= a1)
-		a2-=a1;
-	else
-		a2+=a1;
+    if (a2 >= a1)
+        a2-=a1;
+    else
+        a2+=a1;
 
-	new_counter+=a2;
-
+    new_counter+=a2;
 }
 
 
@@ -184,9 +183,9 @@ void clock_r1(u_long i1)
 
 void new_reset_check()
 {
-	 new_counter=1979;
-	 p_ruota1=0;
-	 p_ruota2=2;
+    new_counter=1979;
+    p_ruota1=0;
+    p_ruota2=2;
 }
 
 // --------------------------------------------------------------------------
@@ -194,8 +193,8 @@ void new_reset_check()
 // versione int.
 int new_check_i(int i)
 {
-	 clock_r1(i);
-	 return(i);
+    clock_r1(i);
+    return(i);
 }
 
 // --------------------------------------------------------------------------
@@ -203,8 +202,8 @@ int new_check_i(int i)
 // versione unsigned long.
 u_long new_check_l(u_long i)
 {
-	 clock_r1(i);
-	 return(i);
+    clock_r1(i);
+    return(i);
 }
 
 // --------------------------------------------------------------------------
