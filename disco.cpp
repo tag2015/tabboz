133025ecc83e700e7afe643434c24334adaeef2e
Walter Agazzi <tag2015@gmail.com>
2022-03-31 17:32:21 +0200

    Implemented disco.h/disco.c

--- disco.cpp
+++ disco.cpp
@@ -1,53 +1,118 @@
-// Tabboz Simulator
-// (C) Copyright 1997-1999 by Andrea Bonomi
-// 6 Aprile 1999 - Inizio implementazione lettore CDROM
-// 30 Maggio 1999 - Conversione Tabbozzo -> Tabbozza
+/*             Tabboz Simulator             */
+/* (C) Copyright 1997-2000 by Andrea Bonomi */
+
+/*
+    This file is part of Tabboz Simulator.
+
+    Tabboz Simulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tabboz Simulator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tabboz Simulator.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "os.h"
-
-// Lettore di CD-ROM per Windows... (cagata !)
-// #define CDROM
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#ifdef TABBOZ_WIN
-// #ifdef CDROM
-// #include <mmsystem.h>
-// #endif
-#endif
+/* Header per toolkit FLTK */
+#include <FL/Fl.H>
+#include <FL/fl_ask.H>
+
+/* L'opzione CDROM è stata tolta... verrà sostituita dalla possibilità di */
+/* riprodurre file audio personali (in futuro...)                         */
 
 #include "zarrosim.h"
-static char sccsid[] = "@(#)" __FILE__ " " VERSION " (Andrea Bonomi) " __DATE__;
-static int  numdisco;
+#include "debug.h"
+
+#include "disco.h"
+#include "calendario.h"
+
+//static char sccsid[] = "@(#)" __FILE__ " " VERSION " (Andrea Bonomi) " __DATE__;
 
 
-// Per una questione di svogliatezza del programmatore, viene usata STSCOOTER
-// anche x i vestiti e per le discoteche.
-
-STSCOOTER DiscoMem[] =
-    {    { 0,  0, 0, 0, 0, 0,   0,  0,"---"},
-         { 0, 30, 2,15, 1, 0,  36,  0,""   },
-         { 0,  0, 1, 7, 4, 0,  26,  0,""   },
-         { 0,  0, 1, 8, 1, 0,  30,  0,""   },
-         { 0, 35, 3,15, 1, 0,  36,  0,""   },
-         { 0,  0, 2, 6, 3, 0,  26,  0,""   },
-         { 0,  0, 2, 5, 2, 0,  22,  0,""   },
-         { 0,  0, 3, 8, 1, 0,  30,  0,""   },
-         { 1,  0, 2, 9, 7, 0,  36,  0,""   } };
-//         |   |  |  |   \       \costo
-//         |   |  |  |    \giorno di chiusura (1=lunedi',etc... - 0=nessuno)
-//         \   \  \  \incremento fama
+const STDISCO DiscoMem[] =
+    {    { 0,  0, 0,  0, 0,  0,  "---"},
+         { 0, 30, 2, 15, 1, 36,  ""   },
+         { 0,  0, 1,  7, 4, 26,  ""   },
+         { 0,  0, 1,  8, 1, 30,  ""   },
+         { 0, 35, 3, 15, 1, 36,  ""   },
+         { 0,  0, 2,  6, 3, 26,  ""   },
+         { 0,  0, 2,  5, 2, 22,  ""   },
+         { 0,  0, 3,  8, 1, 30,  ""   },
+         { 1,  0, 2,  9, 7, 36,  ""   } };
+//         |   |  |   |  |   |     \nome  
+//         |   |  |   |   \   \costo
+//         |   |  |   |    \giorno di chiusura (1=lunedi',etc... - 0=nessuno)
+//         \   \  \   \incremento fama
 //          \   \  \incremento reputazione
 //           \   \figosita' minima x entrare (selezione all' ingresso)
 //            \1=disco fuori porta - ci puoi arrivare solo se hai lo scooter...
 
 
+/* Routine per il pagamento della Discoteca */
+void PagaDisco(int scelta)
+{
+    char tmp[128];
+    int prezzo;
 
+    if ( (DiscoMem[scelta].fuoriporta) && (ScooterData.stato == -1) ) {
+        fl_message_title("Discoteca fuori porta");
+        fl_alert("Senza lo scooter non puoi andare nelle discoteche fuori porta...");
+        //Evento(hInstance); FIXME Eventi
+        return;
+    }
+    
+    if (DiscoMem[scelta].g_chiusura == x_giornoset) {
+        fl_message_title("Giorno di chiusura");
+        fl_alert("Un cartello recita che oggi è il giorno di chiusura settimanale...");
+        return;
+    }
+
+    if (sesso == 'M')
+        prezzo=DiscoMem[scelta].costo;
+    else
+        prezzo=DiscoMem[scelta].costo - 10;  // ingresso ridotto
+
+    if (prezzo > Soldi)    // check costo
+        nomoney(DISCO);
+    else
+        if ((DiscoMem[scelta].fama > Fama) && (sesso == 'M')) {    // check selezione all'ingresso
+            if (sound_active) //TabbozPlaySound(302); FIXME Suoni
+            fl_message_title("Selezione all'ingresso");
+            fl_alert("Mi dispiace signore, conciato così, qui non può entrare...\nVenga vestito meglio la prossima volta, signore.");
+            if (Reputazione > 2) Reputazione--;
+            if (Fama > 2) Fama--;
+        } else {
+            if (sound_active) // TabbozPlaySound(303 + random(3));  // suoni: 0303 -> 0305 FIXME SUONI
+            Soldi-= prezzo;
+            #ifdef LOGGING
+                sprintf(tmp,"discoteca: Paga %s",MostraSoldi(DiscoMem[scelta].costo));
+                writelog(tmp);
+            #endif
+            Fama+=DiscoMem[scelta].fama_inc;  //BUGFIX qui incrementava con la fama della disco anzichè il bonus
+            Reputazione+=DiscoMem[scelta].rep_inc;
+            if (Fama > 100) Fama=100;
+            if (Reputazione > 100) Reputazione=100;
+        }
+    //Evento(hInstance); FIXME Evento
+}
+
+
+
+#ifdef DEADCODE
 /********************************************************************/
-/* Routine per il pagamento della Discoteca.
+/* Routine per il pagamento della Discoteca.                        */
 /********************************************************************/
 
 static void PagaDisco(HANDLE hInstance)
@@ -190,3 +255,4 @@ BOOL FAR PASCAL Disco(HWND hDlg, WORD message, WORD wParam, LONG lParam)
     }
     return(FALSE);
 }
+#endif
---
+++ disco.h
@@ -0,0 +1,33 @@
+/*             Tabboz Simulator             */
+/* (C) Copyright 1997-2000 by Andrea Bonomi */
+
+/*
+    This file is part of Tabboz Simulator.
+
+    Tabboz Simulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tabboz Simulator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tabboz Simulator.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+typedef struct tagSTDISCO {
+    bool        fuoriporta;     // raggiungibile solo con scooter
+    int         fama;           // figosita' minima x entrare (selezione all' ingresso)
+    int         rep_inc;        // incremento reputazione
+    int         fama_inc;       // incremento figosità
+    int         g_chiusura;     // giorno di chiusura (1=lunedi... 0=nessuno)
+    int         costo;          // costo ingresso intero
+    char        nome[30];       // nome della disco
+} STDISCO;
+
+extern const STDISCO  DiscoMem[];
+
+void PagaDisco(int scelta);
