e8ab5c7685bee48cf79398d444a6018b75c80233
Walter Agazzi <tag2015@gmail.com>
2022-04-26 13:28:46 +0200

    Add copyright

--- sharedimg.cpp
+++ sharedimg.cpp
@@ -1,7 +1,27 @@
+/*             Tabboz Simulator             */
+/* (C) Copyright 1997-2000 by Andrea Bonomi */
+
+/*
+    This file is part of Tabboz Simulator.
+
+    Tabboz Simulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tabboz Simulator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tabboz Simulator.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "os.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-
 
 #include "sharedimg.h"
 
@@ -17,6 +37,8 @@ Fl_Shared_Image *ImgDitte[N_DITTE];
 
 Fl_Shared_Image *ImgExit;
 
+
+/* Carica da file le immagini condivise (usate in più finestre) */
 void CaricaSharedImgs()
 {
     char path[128];
