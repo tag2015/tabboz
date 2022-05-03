f5fe8124f58c9b47d80c3b60046d897fe268d2da
Walter Agazzi <tag2015@gmail.com>
2022-05-03 11:53:18 +0200

    New small routine to change msg icons

---
+++ dialogs.cpp
@@ -0,0 +1,58 @@
+/*             Tabboz Simulator             */
+/* (C) Copyright 1997-2000 by Andrea Bonomi */
+
+/*
+    This file is part of Tabboz Simulator.
+
+    Tabboz Simulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tabboz Simulator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tabboz Simulator.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "zarrosim.h"
+#include "sharedimg.h"
+
+#include "dialogs.h"
+
+
+/* Ottiene un puntatore all'icona delle dialog, e "cancella" l'icona predefinita */
+void MsgInit()
+{
+    Fl_Box *icona_msg = (Fl_Box *) fl_message_icon();
+    icona_msg->copy_label("");
+    icona_msg->box(FL_NO_BOX);
+    icona_msg->align(FL_ALIGN_IMAGE_BACKDROP);
+}
+
+
+/* Cambia icona della prossima dialog */
+void MsgIcona(int scelta)
+{
+    Fl_Box *icona_msg = (Fl_Box *) fl_message_icon();
+    icona_msg->show();
+    
+    switch(scelta) {
+        case 0: icona_msg->hide();
+                break;
+        case 1: icona_msg->image(ImgInfo);
+                break;
+        case 2: icona_msg->image(ImgAvviso);
+                break;
+        case 3: icona_msg->image(ImgStop);
+                break;
+        case 4: icona_msg->image(ImgDomanda);
+                break;
+        case 5: icona_msg->image(ImgCool);
+                break;
+    }
+
+}
---
+++ dialogs.h
@@ -0,0 +1,16 @@
+
+
+/* Header per toolkit FLTK */
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+
+#define ICONA_NO 0
+#define ICONA_INFO 1
+#define ICONA_AVVISO 2
+#define ICONA_STOP 3
+#define ICONA_DOMANDA 4
+#define ICONA_COOL 5
+
+
+void MsgInit();
+void MsgIcona(int);
