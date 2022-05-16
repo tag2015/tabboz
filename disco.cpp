002f64afbff9243ecde2ca28f4dbff04ae7802ef
Walter Agazzi <tag2015@gmail.com>
2022-05-16 23:02:05 +0200

    Change bonuses in disco

--- disco.cpp
+++ disco.cpp
@@ -44,23 +44,24 @@
 
 
 
-const STDISCO DiscoMem[] =
-    {    { 0,  0, 0,  0, 0,  0,  "---"},
-         { 0, 30, 2, 15, 1, 36,  ""   },
-         { 0,  0, 1,  7, 4, 26,  ""   },
-         { 0,  0, 1,  8, 1, 30,  ""   },
-         { 0, 35, 3, 15, 1, 36,  ""   },
-         { 0,  0, 2,  6, 3, 26,  ""   },
-         { 0,  0, 2,  5, 2, 22,  ""   },
-         { 0,  0, 3,  8, 1, 30,  ""   },
-         { 1,  0, 2,  9, 7, 36,  ""   } };
-//         |   |  |   |  |   |     \nome  
-//         |   |  |   |   \   \costo
-//         |   |  |   |    \giorno di chiusura (1=lunedi',etc... - 0=nessuno)
-//         \   \  \   \incremento fama
-//          \   \  \incremento reputazione
-//           \   \figosita' minima x entrare (selezione all' ingresso)
-//            \1=disco fuori porta - ci puoi arrivare solo se hai lo scooter...
+const STDISCO DiscoMem[] = {
+    { 0,  0, 0,  0, 0,  0,  "---"},
+    { 0, 30, 2,  5, 1, 36,  ""   },
+    { 0,  0, 2,  1, 4, 26,  ""   },
+    { 0,  0, 2,  2, 1, 30,  ""   },
+    { 0, 35, 3,  5, 1, 36,  ""   },
+    { 0,  0, 2,  1, 3, 26,  ""   },
+    { 0,  0, 1,  1, 2, 22,  ""   },
+    { 0,  0, 2,  3, 1, 30,  ""   },
+    { 1,  0, 2,  4, 7, 36,  ""   }
+};//  |   |  |   |  |   |    |
+//    |   |  |   |  |   |     \nome  
+//    |   |  |   |   \   \costo
+//    |   |  |   |    \giorno di chiusura (1=lunedi',etc... - 0=nessuno)
+//    \   \  \   \incremento fama
+//     \   \  \incremento reputazione
+//      \   \figosita' minima x entrare (selezione all' ingresso)
+//       \1=disco fuori porta - ci puoi arrivare solo se hai lo scooter...
 
 
 
